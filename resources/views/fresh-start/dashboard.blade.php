cb2f794ec30443e4856ba87a8b3ce034f8480ed6
Mark Ryan <MarkRyan@tsla-mbp15.local>
2018-06-01 11:58:27 +0100

    not sure why commit

--- resources/views/fresh-start/dashboard.blade.php
+++ resources/views/fresh-start/dashboard.blade.php
@@ -150,7 +150,7 @@
       </div>
     </div>
     <div class="col-12 col-lg-8 center-align">
-      <div class="card-panel white" id="welcome-video" style="height: auto;">
+      <div class="card-panel white" id="welcome-video" style="height: autogit ;">
         <div class="video-container">
           <video  width="100%" height="auto" controls>
            <source src="{{url('/videos/Welcome-to-the-Fresh-Start-compressed.mp4')}}" type="video/mp4">
