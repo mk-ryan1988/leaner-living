468ff83f27eb6aaaab277e4fe446095d4fd07aa3
Mark Ryan <MarkRyan@tsla-mbp15.local>
2018-07-02 11:46:23 +0100

    testing

--- app/Http/Controllers/stripeController.php
+++ app/Http/Controllers/stripeController.php
@@ -20,58 +20,51 @@ class stripeController extends Controller
       $settings = Setting::first();
       $secretKey = Crypt::decryptString($settings->stripe_key_secret);
 
-      // $order = new Payment;
-      // $order->user_id = Auth::id();
-      // $order->product = 'fresh-start';
-      // $order->card_name = $request->cardName;
-      // $order->commence_at = now()->addMonth()->startOfMonth();
-      // $order->save();
-      //
+      $order = new Payment;
+      $order->user_id = Auth::id();
+      $order->product = 'fresh-start';
+      $order->card_name = $request->cardName;
+      $order->commence_at = now()->addMonth()->startOfMonth();
+      $order->save();
+
       // $parQ = ParQ::where('name', Auth::user()->name)->first();
       // $parQ->user_id = Auth::id();
       // $parQ->save();
 
-      // $result = $order->id;
+      $result = $order->id;
 
-      // if ($result) {
-      //   // Set your secret key: remember to change this to your live secret key in production
-      //   // See your keys here: https://dashboard.stripe.com/account/apikeys
-      //   \Stripe\Stripe::setApiKey($secretKey);
-      //
-      //   // Token is created using Checkout or Elements!
-      //   // Get the payment token ID submitted by the form:
-      //   $token = $_POST['stripeToken'];
-      //
-      //   $charge = \Stripe\Charge::create([
-      //       'amount' => $settings->freshStart_price,
-      //       'currency' => 'GBP',
-      //       'description' => 'Fresh Start Charge: Mark Testing',
-      //       'source' => $token,
-      //       'receipt_email' => 'mkryan1988@gmail.com',
-      //       "metadata" => array("order_id" => $result)
-      //   ]);
-      //
-      //   // $paid = User::find(Auth::id());
-      //   // $paid->paid = 1;
-      //   // $paid->paid_at = now();
-      //   // $paid->save();
-      //
-      //   $mailData = array(
-      //                'name'     => 'Mark Ryan',
-      //                'email'    => 'mkryan1988@gmail.com',
-      //               );
-      //   Mail::to('admin@leaner-living.com')->send(new paymentConfirm($mailData));
-      //
-      //   return redirect()->route('fresh-start.questionnaire')->with('success', 'Payment Accepted!');
-      //
-      // }
-      $mailData = array(
-                   'name'     => 'Mark Ryan',
-                   'email'    => 'mkryan1988@gmail.com',
-                  );
-      Mail::to('mkryan1988@gmail.com')->send(new paymentConfirm($mailData));
+      if ($result) {
+        // // Set your secret key: remember to change this to your live secret key in production
+        // // See your keys here: https://dashboard.stripe.com/account/apikeys
+        // \Stripe\Stripe::setApiKey($secretKey);
+        //
+        // // Token is created using Checkout or Elements!
+        // // Get the payment token ID submitted by the form:
+        // $token = $_POST['stripeToken'];
+        //
+        // $charge = \Stripe\Charge::create([
+        //     'amount' => $settings->freshStart_price,
+        //     'currency' => 'GBP',
+        //     'description' => 'Fresh Start Charge: Mark Testing',
+        //     'source' => $token,
+        //     'receipt_email' => 'mkryan1988@gmail.com',
+        //     "metadata" => array("order_id" => $result)
+        // ]);
 
-      return redirect()->route('fresh-start.questionnaire')->with('success', 'Payment Accepted!');
+        $paid = User::find(Auth::id());
+        $paid->paid = 1;
+        $paid->paid_at = now();
+        $paid->save();
+
+        $mailData = array(
+                     'name'     => Auth::user()->name,
+                     'email'    => Auth::user()->email,
+                    );
+        Mail::to('admin@leaner-living.com')->send(new paymentConfirm($mailData));
+
+        return redirect()->route('fresh-start.questionnaire')->with('success', 'Payment Accepted!');
+
+      }
 
     }
     public function test(Request $request) {
